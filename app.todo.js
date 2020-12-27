4a1ebbd63a45e1bfc62c61979eccef0540018ab5
Thanayut T <30551284+tithanayut@users.noreply.github.com>
2020-12-27 22:39:51 +0700

    Implement localStorage & fix bugs

--- app.todo.js
+++ app.todo.js
@@ -2,13 +2,31 @@ const btnAdd = document.getElementById("addBtn");
 const listInput = document.getElementById("listInput");
 const list = document.getElementById("list");
 
-btnAdd.addEventListener("click", function(){
-    if(listInput.value !== ""){
+if(localStorage.getItem("tasks") === null){
+    const initTask = ["Eat Apple", "Drink Kiwi"];
+    localStorage.setItem("tasks", JSON.stringify(initTask));
+}
 
+const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+storedTasks.forEach(function(t){
+    const delbtn = document.createElement("button");
+    delbtn.className = "delBtn";
+    delbtn.innerText = "Del";
+
+    const elementToAdd = document.createElement("li");
+    elementToAdd.innerText = t + " ";
+    elementToAdd.appendChild(delbtn);
+
+    list.appendChild(elementToAdd);
+});
+
+btnAdd.addEventListener("click", function(){
+    listInput.value = listInput.value.trim();
+    if(listInput.value !== ""){
         let isInList = false;
         const inlist = Array.from(list.children);
         inlist.forEach(function(f){
-            const txtCompare = f.innerHTML.replace(" <button id=\"delBtn\">Del</button>", "");
+            const txtCompare = f.innerHTML.replace(" <button class=\"delBtn\">Del</button>", "");
             if(listInput.value === txtCompare){
                 isInList = true;
             }
@@ -16,7 +34,7 @@ btnAdd.addEventListener("click", function(){
 
         if(isInList === false){
             const delbtn = document.createElement("button");
-            delbtn.id = "delBtn";
+            delbtn.className = "delBtn";
             delbtn.innerText = "Del";
 
             const elementToAdd = document.createElement("li");
@@ -24,20 +42,26 @@ btnAdd.addEventListener("click", function(){
             elementToAdd.appendChild(delbtn);
 
             list.appendChild(elementToAdd);
+
+            storedTasks.push(listInput.value);
+            localStorage.setItem("tasks", JSON.stringify(storedTasks));
+
+            listInput.value = "";
         }else{
             alert("Input already in list");
         }
-
-        listInput.value = "";
-        
     }else{
         alert("Please enter listInput");
     }
 });
 
 document.body.addEventListener("click", function(e){
-    if(e.target.id === "delBtn"){
+    if(e.target.className === "delBtn"){
         e.target.parentElement.remove();
+        
+        const taskText = e.target.parentElement.innerHTML.replace(" <button class=\"delBtn\">Del</button>", "");
+        const toRemoved = storedTasks.indexOf(taskText);
+        storedTasks.splice(toRemoved, 1);
+        localStorage.setItem("tasks", JSON.stringify(storedTasks));
     }
 })
-
